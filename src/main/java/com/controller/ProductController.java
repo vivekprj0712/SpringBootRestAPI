4fd98dec7f1daed214079fface06d48050cfc616
vivekprj0712 <prajapativivek949@gmail.com>
2024-09-23 14:42:16 +0530

    addProduct

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
+import com.repository.ProductRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,17 +13,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class ProductController {
 
+	@Autowired
+	ProductRepository productRepository;
+
+	
+	
 	//api 
 	// read product information and store into db
 	
 	@PostMapping("/products")
-	public String addProduct(@RequestBody ProductEntity productEntity) {
+	public ProductEntity addProduct(@RequestBody ProductEntity productEntity) {
 		
 		System.out.println(productEntity.getProductName());
 		System.out.println(productEntity.getCategory());
-		return "success";
+		productRepository.save(productEntity);
+		return productEntity;
 	}
 	
 }
 
-//url :- http://localhost:8899/products
+//url :- http://localhost:8899/products	
