7261f4131ead12aa56f443f084dbef785f1e6c28
vivekprj0712 <prajapativivek949@gmail.com>
2024-09-24 11:33:26 +0530

    product read , delete and updates,

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.entity.ProductEntity;
 import com.repository.ProductRepository;
 
+import java.security.PublicKey;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -29,7 +39,62 @@ public class ProductController {
 		productRepository.save(productEntity);
 		return productEntity;
 	}
+	//api
+	// read all products information 
+	@GetMapping("/products")
+	public List<ProductEntity> getAllProducts(ProductEntity productEntity){	
+	  List<ProductEntity>	products = productRepository.findAll();
+		return products;
+	}
 	
+	// read product by Id
+	//@GetMapping("/products/:productId") 
+	@GetMapping("/products/{productId}")
+	public ProductEntity getProductById(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> product = productRepository.findById(productId);
+		if(product.isEmpty()) {
+			return null;
+		}else {
+			ProductEntity productEntity = product.get();
+			return productEntity;
+		}
+	}
+	
+	//using @RequestParam
+	@GetMapping("/productsById")
+	public ProductEntity getProductById2(@RequestParam("productId") Integer productId) {
+		Optional<ProductEntity> product = productRepository.findById(productId);
+		if(product.isEmpty()) {
+			return null;
+		}else {
+			ProductEntity productEntity = product.get();
+			return productEntity;
+		}
+	}
+	
+	
+	//Api -> remove product from the products table -> delete product
+	//input -> productId
+	@DeleteMapping("products/{productId}")
+	public String deleteProductById(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> product = productRepository.findById(productId);
+		if(product.isEmpty()) {
+			return "Not Found";
+		}else {
+			productRepository.deleteById(productId);
+			return "Delete Success";
+		}
+	}
+	
+	//update
+	// input all product info that we can modify , product id must be present 
+	
+	@PutMapping("/products")
+	public String updateProduct(@RequestBody ProductEntity productEntity) {
+		
+		productRepository.save(productEntity); // id is present 
+		return "updated";
+	}
 }
 
 //url :- http://localhost:8899/products	
